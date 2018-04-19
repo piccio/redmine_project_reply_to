9127dfc2f81f0afea4d6d34c3c332b1688bb4b38
Roberto Piccini <roberto.piccini@gmail.com>
2018-04-19 16:34:16 +0200

    redmine version 3.x

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine_project_reply_to/mailer_patch'
 
 Rails.configuration.to_prepare do
   unless Mailer.included_modules.include? RedmineProjectReplyTo::MailerPatch
-    Mailer.send(:include, RedmineProjectReplyTo::MailerPatch)
+    Mailer.prepend(RedmineProjectReplyTo::MailerPatch)
   end
 end
 
@@ -10,10 +10,9 @@ Redmine::Plugin.register :redmine_project_reply_to do
   name 'Redmine Project Reply To plugin'
   author 'Roberto Piccini'
   description %q{set a own 'reply_to' field for every project}
-  version '0.0.1'
+  version '2.0.0'
   url 'https://github.com/piccio/redmine_project_reply_to'
   author_url 'https://github.com/piccio'
-  requires_redmine version: '2.6.0'
 
-  settings default: { reply_to_field: nil }, partial: 'settings/project_reply_to'
+  settings default: { 'reply_to_field' => nil }, partial: 'settings/project_reply_to'
 end
